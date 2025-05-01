955de74b9137a20485a6029b9ff6733e5694f5c6
belaja-akacija <oselskyi@protonmail.com>
2025-05-01 02:52:58 -0500

    added view class

--- Drawing.CenterView.Library/CurrentDrawing.cs
+++ Drawing.CenterView.Library/CurrentDrawing.cs
@@ -1,14 +1,25 @@
+using System.Collections;
+
 namespace Drawing.CenterView.Library;
 
-public class CurrentDrawing{
+public class CurrentDrawing
+{
 
-  public Tekla.Structures.Drawing.Drawing Drawing {get; set;}
-  public Tekla.Structures.Drawing.DrawingObjectEnumerator Views {get; set;}
-  public bool IsValidForCenter {get; set;}
+    public Tekla.Structures.Drawing.Drawing Drawing { get; set; }
+    public Tekla.Structures.Drawing.DrawingObjectEnumerator Views { get; set; }
+    public bool IsValidForCenter { get; set; }
+    private List<Tekla.Structures.Drawing.View> ValidViews { get; set; } = new List<Tekla.Structures.Drawing.View>();
 
-  public CurrentDrawing(Tekla.Structures.Drawing.Drawing drawing)
-  {
-    Drawing = drawing;
-    Views = Drawing.GetSheet().GetViews();
-  }
+    public CurrentDrawing(Tekla.Structures.Drawing.Drawing drawing)
+    {
+        Drawing = drawing;
+        Views = Drawing.GetSheet()
+                       .GetViews();
+    }
+
+    private void TestMethod()
+    {
+        var viewsCount = ValidViews.Count;
+        IsValidForCenter = (viewsCount > 1) ? false : true;
+    }
 }

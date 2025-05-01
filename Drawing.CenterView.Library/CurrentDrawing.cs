5ee5a3de1682f467e928f1d3f27b019fe5cb9d30
eliza-oselskyi <ellie.oselskyi@kirbybuildingsystems.com>
2025-05-01 11:40:34 -0500

    Drawing class updated
    Signed-off-by: eliza-oselskyi <ellie.oselskyi@kirbybuildingsystems.com>

--- Drawing.CenterView.Library/CurrentDrawing.cs
+++ Drawing.CenterView.Library/CurrentDrawing.cs
@@ -6,20 +6,21 @@ public class CurrentDrawing
 {
 
     public Tekla.Structures.Drawing.Drawing Drawing { get; set; }
-    public Tekla.Structures.Drawing.DrawingObjectEnumerator Views { get; set; }
-    public bool IsValidForCenter { get; set; }
-    private List<Tekla.Structures.Drawing.View> ValidViews { get; set; } = new List<Tekla.Structures.Drawing.View>();
+    public string Name { get; set; }
+    private string _title3;
+    public Type DrawingType { get; set; }
+    private Tekla.Structures.Drawing.DrawingObjectEnumerator Views { get; set; }
+    public List<Tekla.Structures.Drawing.View> ValidViews { get; } = [];
+    public bool Excluded { get; }
+    private bool _guiMode;
+    
 
     public CurrentDrawing(Tekla.Structures.Drawing.Drawing drawing)
     {
         Drawing = drawing;
+        DrawingType = Drawing.GetType();
         Views = Drawing.GetSheet()
                        .GetViews();
     }
 
-    private void TestMethod()
-    {
-        var viewsCount = ValidViews.Count;
-        IsValidForCenter = (viewsCount > 1) ? false : true;
-    }
 }
